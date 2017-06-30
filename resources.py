691e7830ac01e60ad5066387725d5132fbc4c0a5
monodo <olivier.monod@ne.ch>
2017-06-30 17:56:30 +0200

    fix

--- resources.py
+++ resources.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.7)
+# Created: ven. 30. juin 16:10:25 2017
+#      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
