36454cf936bac85a38d0121a20f56f0bfc1e3e16
monodo <olivier.monod@ne.ch>
2017-07-24 10:52:11 +0200

    for the record, store the configuration table structure

---
+++ datamodel/selvansgeo.sql
@@ -0,0 +1,27 @@
+-- Table: selvansgeo.analysis
+
+-- DROP TABLE selvansgeo.analysis;
+
+CREATE TABLE selvansgeo.analysis
+(
+  id integer NOT NULL,
+  analysis_name character varying(256),
+  querystring text,
+  default_symbology character varying(256),
+  join_target_pkfield character varying(256),
+  join_source_fkfield character varying(256),
+  field_of_interest character varying(256),
+  join_target_table character varying(256),
+  join_target_schema character varying(256),
+  field_of_interest_type character varying(256),
+  date_filtering boolean,
+  pie_chart boolean,
+  pie_chart_colors text,
+  datefield character varying(256),
+  timerange_filtering boolean,
+  coupetype_filtering boolean,
+  CONSTRAINT analysis_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=TRUE
+);
